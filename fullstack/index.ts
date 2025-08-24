246341eb98023350043230cc56279f7377390230
Jayampathy Balasuriya <jam@vfullstack.com>
2025-08-24 13:01:15 +0530

    configuration to runmultiple apps

--- fullstack/index.ts
+++ fullstack/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently"
+
+concurrently([
+    {
+        command: "bun run dev",
+        name: "server",
+        cwd: "packages/server",
+        prefixColor: "cyan"
+    },
+    {
+        command: "vite",
+        name: "client",
+        cwd: "packages/client",
+        prefixColor: "yellow"
+    }
+])
